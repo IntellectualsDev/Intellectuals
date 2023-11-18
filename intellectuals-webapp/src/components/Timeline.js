b70e7f353db04e866a8e621895ff68ab9c30f3f3
anshuljg07 <anshuljg07@gmail.com>
2023-11-18 10:21:21 +0530

    Timeline strucure established, styling IP

--- intellectuals-webapp/src/components/Timeline.js
+++ intellectuals-webapp/src/components/Timeline.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import {Card, CardBody} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import '../css/Timeline.css'; // Make sure to create this CSS file for styling
 
 const Timeline = ({ teamMembers }) => {
     return (
         <div className="timeline-container">
-            {/* This is the vertical line of the timeline */}
             <div className="timeline-line"></div>
-
             {teamMembers.map((member, index) => (
                 <div key={member.id} className="timeline-item">
-                    {/* The circle on the timeline */}
-                    <div className={`timeline-icon ${index % 2 === 0 ? 'left' : 'right'}`}></div>
-
-                    <div className={`profile-container ${index % 2 === 0 ? 'left' : 'right'}`}>
-                        {/* Profile will go here */}
+                    <div className={`profile-container`}>
                         <img src={member.image} alt={member.name} className="profile-image"/>
                         <h3>{member.name}</h3>
                         <p>{member.role}</p>
                     </div>
-
-                    <div className={`projects-container ${index % 2 === 0 ? 'right' : 'left'}`}>
-                        {/* Project cards will go here */}
+                    <div className="timeline-icon"></div>
+                    <div className={`projects-container card-group-scrollable`}>
                         {member.projects.map(project => (
-                            <Card key={project.id} style={{ width: '18rem' }}>
+                            <Card key={project.id} style={{ minWidth: '18rem' }}>
                                 <Card.Img variant="top" src={project.image} />
                                 <Card.Body>
                                     <Card.Title>{project.title}</Card.Title>
